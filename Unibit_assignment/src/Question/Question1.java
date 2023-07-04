7cb59a8e60d34b412ab297d2aa779052d3c82be7
Aman Singh <105989482+singhaman8859@users.noreply.github.com>
2023-07-05 03:08:38 +0530

    Update Question1.java

--- Unibit_assignment/src/Question/Question1.java
+++ Unibit_assignment/src/Question/Question1.java
@@ -2,9 +2,14 @@ package Question;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Question1 {
+	
+	// brute force approach for finding the combinations of elements whose sum = target value 
+	// time complexity of this function will be O(n^2);
 
 	public static List<List<Integer>> firstCombination(int[] arr, int target) {
 		List<List<Integer>> res = new ArrayList<>();
@@ -21,6 +26,41 @@ public class Question1 {
 
 		return res;
 	}
+	
+	// this method is done with the help of hashmap for finding the combination of 2 values whose sum target 
+	// but the time complexity of this function will be O(n+K)
+	public static int[][] firstCombinations2(int[] nums, int target) {
+		Map<Integer, List<Integer>> map = new HashMap<>();
+		List<int[]> combinations = new ArrayList<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement)) {
+				List<Integer> indices = map.get(complement);
+				for (int index : indices) {
+					combinations.add(new int[] { nums[index], nums[i] });
+				}
+			}
+			if (!map.containsKey(nums[i])) {
+				map.put(nums[i], new ArrayList<>());
+			}
+			map.get(nums[i]).add(i);
+		}
+
+		int[][] result = new int[combinations.size()][2];
+		for (int i = 0; i < combinations.size(); i++) {
+			result[i] = combinations.get(i);
+		}
+
+		return result;
+	}
+	
+	// this function is for find the combination of doubled target 
+	// as this mention that first we have to find the combination of two element and then we have to merge
+	// those elements only and also the array provided in this function is already sorted in ascending order
+	// so if the 2 elements sum = target and i have to find the combination of target*2 so 4 elements sum definetly = target*2
+	//same thing i have did i am taking 2 elements from starting and 2 from last because arr is sorted
+	// this function will return the uniq elements combination of doubled target 
 
 	public static List<List<Integer>> secondCombination(int[] arr, int target) {
 		List<List<Integer>> res = new ArrayList<>();
